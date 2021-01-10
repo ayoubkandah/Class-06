16a2ea6c2d46c188164eefa47c7adb12702d81a1
ayoubkandah <a.kandah994@gmail.com>
2021-01-10 16:12:45 +0200

    firstly

---
+++ JS/app.js
@@ -0,0 +1,57 @@
+
+
+
+
+var Seattle = {
+    
+minPh:23,
+maxPh:65,
+avgCs:6.3,
+Sales:[],
+ randomCusNum:function () {
+     
+     var ResultRand= Math.floor(Math.random()*(Seattle.maxPh-Seattle.minPh +1)+Seattle.minPh);
+return ResultRand;
+},
+
+SalePeHo:function () {
+    var SalesPerHour=[];
+for(x=0;x<14;x++){
+SalesPerHour.push(Math.floor(Seattle.randomCusNum()*Seattle.avgCs));
+
+}
+Seattle.Sales=SalesPerHour;
+//return [SalesPerHour];
+}
+};
+
+
+Seattle.SalePeHo();
+seatlelist();
+
+function seatlelist(){
+    
+    var ULlist= document.getElementById("UL");
+    var hours=6;
+for(x=0;x<14;x++){
+ var list=document.createElement("li");
+
+var displayhour;
+var PMorAM;
+if (hours>0&&hours<12){
+    PMorAM="AM";
+    displayhour=hours;
+}else if (hours>12){
+    PMorAM="PM";
+    displayhour=hours-12;
+} else if (hours=12){
+    PMorAM="PM";
+    displayhour=12;
+}
+list.textContent=displayhour+" "+PMorAM+":  "+Seattle.Sales[x]+" coockies";
+ULlist.appendChild(list);
+hours++;
+}
+
+}
+
